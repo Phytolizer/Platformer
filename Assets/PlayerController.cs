0387ed94222ebd76272fb1ac0cb82b1aae9ffee8
kyle.coffey <kyle.coffey@tamu.edu>
2020-12-23 14:06:43 -0600

    basic forces

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+
+    private CharacterController _controller;
+
+    private const float speed = 3;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.useGravity = false;
+        _controller = GetComponent<CharacterController>();
     }
 
+    private bool Grounded => Physics.Raycast(transform.position, Vector3.down, _controller.height / 2 + 0.01f);
+
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            _rigidbody.AddForce(speed * Vector3.right);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            _rigidbody.AddForce(speed * Vector3.left);
+        }
+
+        if (Grounded)
+        {
+            var v = _rigidbody.velocity;
+            v.y = 0;
+            _rigidbody.velocity = v;
+        }
+        else
+        {
+            _rigidbody.velocity += 0.1f * Vector3.down;
+        }
     }
 }
